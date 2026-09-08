b515acd881a83f3bbedf9a8ab33a9a1cb43b6c64
Khairul Asyrani bin Sulaiman <bezicto@gmail.com>
2026-09-09 05:52:55 +0800

    Untrack site_dev and .DS_Store, add .gitignore

--- site_dev/blocks/index.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE HTML><html lang='en'>
-<head><title>403 Forbidden</title></head>
-<body>
-<h1>Forbidden</h1>
-<div>You don't have permission to access this directory on this server.</div>
-</body>
-</html>
